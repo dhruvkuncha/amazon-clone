d58a64a985ff9be78f24d1e17841d302eeaacca0
Dhruv Kuncha <kunchadhruv03@outlook.com>
2022-04-11 22:35:43 -0500

    home page

--- src/Home.js
+++ src/Home.js
@@ -14,16 +14,32 @@ function Home() {
 
         <div className="home__row">
           <Product
-          title="Rayban"
+          id="1123"
+          title="Ray_Ban New Wayfarer Sunglasses (Matte Black Frame 55mm), Matte Black Frame Solid Black G15 Lens, 55 mm"
           price={199.99}
           image="https://m.media-amazon.com/images/I/51+TKek67sL._AC_UX695_.jpg" 
-          rating={3}/>
-          <Product />
+          rating={4}/>
+          <Product
+          id="1232"
+          title="Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones"
+          rating={5}
+          price={11.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/51-uspgqWIL._SX329_BO1,204,203,200_.jpg" />
         </div>
 
         <div className="home__row">
-          <Product />
-          <Product />
+          <Product
+          id="1123"
+          title="Ray_Ban New Wayfarer Sunglasses (Matte Black Frame 55mm), Matte Black Frame Solid Black G15 Lens, 55 mm"
+          price={199.99}
+          image="https://m.media-amazon.com/images/I/51+TKek67sL._AC_UX695_.jpg" 
+          rating={4}/>
+          <Product
+          id="1232"
+          title="Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones"
+          rating={5}
+          price={11.99}
+          image="https://images-na.ssl-images-amazon.com/images/I/51-uspgqWIL._SX329_BO1,204,203,200_.jpg" /> 
           <Product />
         </div>
 
